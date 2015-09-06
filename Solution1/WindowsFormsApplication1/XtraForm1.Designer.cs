48fee57fe3c08560f7974cfc4b77d3f82156a040
ChuckLu <244657538@qq.com>
2015-09-06 17:21:13 +0800

    设置XtraForm的LookAndFeel的UseDefaultLookAndFeel属性为false

--- Solution1/WindowsFormsApplication1/XtraForm1.Designer.cs
+++ Solution1/WindowsFormsApplication1/XtraForm1.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.LookAndFeel.SkinName = "Caramel";
+            this.LookAndFeel.UseDefaultLookAndFeel = false;
             this.Name = "XtraForm1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "XtraForm1";
