8e2d74225edc68f006e7492904944afe7251da96
ChuckLu <244657538@qq.com>
2016-03-31 15:46:45 +0800

    移除文件中未使用的using

--- Solution1/WindowsFormsApplication1/XtraForm1.cs
+++ Solution1/WindowsFormsApplication1/XtraForm1.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
-using DevExpress.XtraEditors;
+﻿using DevExpress.XtraEditors;
 
 namespace WindowsFormsApplication1
 {
-    public partial class XtraForm1 : DevExpress.XtraEditors.XtraForm
+    public partial class XtraForm1 : XtraForm
     {
         public XtraForm1()
         {
