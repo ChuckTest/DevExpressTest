e9fb44b8ce178ea24d839c0e020288982ec1b52b
ChuckLu <244657538@qq.com>
2016-11-02 14:16:53 +0800

    调整主窗体的尺寸大小

--- Solution1/WindowsFormsApplication1/XtraForm1.Designer.cs
+++ Solution1/WindowsFormsApplication1/XtraForm1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(884, 562);
             this.LookAndFeel.SkinName = "McSkin";
             this.LookAndFeel.UseDefaultLookAndFeel = false;
             this.Name = "XtraForm1";
